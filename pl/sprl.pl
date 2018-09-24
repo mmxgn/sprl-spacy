4d846b4cb6e3c6a2b2013043805f465ad1f13904
Emmanouil Theofanis Chourdakis <e.t.chourdakis@qmul.ac.uk>
2018-09-24 17:10:46 +0100

    Allow adding a tag to the spatial entities after processing

--- pl/sprl.pl
+++ pl/sprl.pl
@@ -43,3 +43,44 @@ sprl_process_sentence(X) :- % Get spatial entity types
 			    assertz(spatial_indicator(SpId)),
 			    assertz(extent(SpId, Sp)),
 			    assertz(spatial_indicator(SRId, SpId)).
+
+sprl_process_sentence(Tag, X) :- % Same as above, but assign also tag:Tag to each spatial entity
+                            % Get spatial entity types
+                            sp_types(Types),
+    
+			    % Resolve sentence to relation
+                            sprl(X, Tr, Sp, Lm, Type),
+			    
+			    % Create spatial relation
+			    new_label(sr, SRId),
+			    assertz(spatial_relation(SRId)),
+
+			    % Assign type
+			    new_label(st, STId),
+			    assertz(srtype(STId)),
+			    assertz(srtype(SRId, STId)),
+			    assertz(gtype(STId, Type)),
+			    
+			    % Add trajector
+			    new_label(tr, TrId),
+			    assertz(trajector(TrId)),
+			    assertz(extent(TrId, Tr)),
+			    assertz(trajector(SRId, TrId)),
+			    closest_type(Tr, Types, TrType),
+			    assertz(type(TrId, TrType)),
+			    assertz(tag(TrId, Label)),
+			    
+			    % Add landmark
+			    new_label(lm, LmId),
+			    assertz(landmark(LmId)),
+			    assertz(extent(LmId, Lm)),
+			    assertz(landmark(SRId, LmId)),
+			    closest_type(Lm, Types, LmType),
+			    assertz(type(LmId, LmType)),
+			    assertz(tag(LmId, Tag)),
+
+			    % Add spatial indicator
+			    new_label(sp, SpId),
+			    assertz(spatial_indicator(SpId)),
+			    assertz(extent(SpId, Sp)),
+			    assertz(spatial_indicator(SRId, SpId)).
