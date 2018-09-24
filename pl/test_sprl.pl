d952c2a5fbb561b827cde47b7cc6bfb6dd8c06bf
Emmanouil Theofanis Chourdakis <e.t.chourdakis@qmul.ac.uk>
2018-09-24 14:14:31 +0100

    added test file for sprl.pl

---
+++ pl/test_sprl.pl
@@ -0,0 +1,13 @@
+:-use_module('sprl.pl').
+
+run_all :- sprl_process_sentence('An angry big dog is behind us.').
+query(run_all).
+query(trajector(X)).
+query(landmark(X)).
+query(spatial_indicator(X)).
+query(type(X,Y)).
+query(extend(X, Extend)).
+query(spatial_relation(X)).
+query(gtype(X,Y)).
+query(srtype(X, Y)).
+query(srtype(X)).
